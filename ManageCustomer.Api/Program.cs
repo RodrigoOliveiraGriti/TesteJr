37e816c82ff1a7cdad667548925418b66d4c44c1
RodrigoGriti <rodrigo_sp_123@hotmail.com>
2025-03-02 15:06:32 -0300

    'Atualização'

--- ManageCustomer.Api/Program.cs
+++ ManageCustomer.Api/Program.cs
@@ -4,13 +4,26 @@ var startup = new Startup(builder.Configuration);
 
 startup.ConfigureServices(builder.Services);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularApp",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200") 
+                  .AllowAnyMethod()
+                  .AllowAnyHeader()
+                  .AllowCredentials();
+        });
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello, world!");
 
+app.UseCors("AllowAngularApp");
+
 app.Urls.Add("http://*:5030");
 
 startup.Configure(app, app.Environment);
-
 
 app.Run();
--- ManageCustomer.Infrastructure/Migrations/20250228115715_create_table_customer.cs
+++ ManageCustomer.Infrastructure/Migrations/20250228115715_create_table_customer.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
@@ -17,9 +17,9 @@ namespace ManageCustomer.Infrastructure.Migrations
                 {
                     Id = table.Column<long>(type: "bigint", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
-                    Nome = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
-                    Email = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
-                    Telefone = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: true),
+                    Nome = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Telefone = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: false),
                     DataNascimento = table.Column<DateTime>(type: "datetime2", maxLength: 11, nullable: false)
                 },
                 constraints: table =>
